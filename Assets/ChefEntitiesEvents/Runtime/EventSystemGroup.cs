8984be1018b258490f781536877a775ae0e4d61d
Cengiz <cengiz.kuscu@chef.gs>
2024-12-18 16:33:26 +0300

    update 106

--- Assets/ChefEntitiesEvents/Runtime/EventSystemGroup.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Entities;
-
-namespace EntitiesEvents
-{
-    [UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
-    [CreateBefore(typeof(SimulationSystemGroup))]
-    public sealed partial class EventSystemGroup : ComponentSystemGroup { }
-}
